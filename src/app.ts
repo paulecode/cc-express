94d62086edf9f1b8941e30cf3081df2f12544dfb
Paul Ebert <paul.ebert@code.berlin>
2024-07-12 17:20:03 +0200

    add /fastapi endpoint

--- src/app.ts
+++ src/app.ts
@@ -20,6 +20,17 @@ app.get("/deploy", (req: Request, res: Response) => {
   res.send("Deployment test 3");
 });
 
+app.get("/fastapi", async (req: Request, res: Response) => {
+  try {
+    const FASTAPI_URL = process.env.FASTAPI_URL || "";
+    const response = await fetch(FASTAPI_URL + "/express");
+    const data = await response.json();
+    res.json({ result: data });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
