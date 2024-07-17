96f3a55cf50b81e1e0212c161af66a3b7a0b592b
Paul Ebert <paul.ebert@code.berlin>
2024-07-18 00:21:24 +0200

    Revert "Revert "added some tests""
    This reverts commit c7e3bb4481182c3694aae7e0dc8955e6b329ee49.

--- src/app.test.ts
+++ src/app.test.ts
@@ -5,3 +5,11 @@ function sum(a: number, b: number) {
 test("Adding 1 and 2 should equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test("Making a request to '/' returns 'Hello World!'", async () => {
+  const response = await fetch("http://localhost:3000/");
+  expect(response.ok).toBeTruthy;
+  expect(response.status).toBe(200);
+  const data = await response.text();
+  expect(data).toBe("Hello World!");
+});
