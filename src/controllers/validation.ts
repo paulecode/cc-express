652e155bd67eec5d15503df416da1cb307e2e266
Paul Ebert <paul.ebert@code.berlin>
2024-07-21 18:36:40 +0200

    add validation lowrecase transform and min and max len

--- src/controllers/validation.ts
+++ src/controllers/validation.ts
@@ -1,6 +1,10 @@
 import z from "zod";
 
 export const loginSchema = z.object({
-  username: z.string(),
-  password: z.string(),
+  username: z
+    .string()
+    .min(4)
+    .max(16)
+    .transform((x) => x.toLowerCase()),
+  password: z.string().min(6).max(64),
 });
