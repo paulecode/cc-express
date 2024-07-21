e75070d9d308adf1cc8245ce73f95c20244efbc1
Paul Ebert <paul.ebert@code.berlin>
2024-07-21 18:37:40 +0200

    create auth route

---
+++ src/routes/auth.ts
@@ -0,0 +1,7 @@
+import express, { Router } from "express";
+import { register } from "../controllers/authController";
+const authRouter = Router();
+
+authRouter.post("/register", register);
+
+export default authRouter;
