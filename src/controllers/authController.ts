b2898c107ee06a5dc74b452a67ba1fc5baac2752
Paul Ebert <paul.ebert@code.berlin>
2024-07-22 16:37:53 +0200

    new login function and register functionality

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,16 +1,39 @@
 import { Request, Response, NextFunction } from "express";
 import { loginSchema } from "./validation";
+import { createAccount } from "../services/authService";
+import { checkAccountExistsByUsername } from "../helpers/accountExists";
 
-async function register(req: Request, res: Response, next: NextFunction) {
+export async function register(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
   const zodResult = loginSchema.safeParse(req.body);
 
   if (zodResult.success) {
     const { username, password } = zodResult.data;
-    res.json({ username, password });
-    console.log(username);
+
+    try {
+      const newId = await createAccount(username, password);
+      res.json({ username, password, newId });
+    } catch (err) {
+      return res.status(400).json({ error: (err as Error).message });
+    }
   } else {
     next(zodResult.error);
   }
 }
 
-export { register };
+export async function login(req: Request, res: Response, next: NextFunction) {
+  const zodResult = loginSchema.safeParse(req.body);
+
+  if (zodResult.success) {
+    const { username, password } = zodResult.data;
+
+    const result = await checkAccountExistsByUsername(username);
+
+    res.json({ result });
+  } else {
+    next(zodResult.error);
+  }
+}
