6839e1d234c85029c5ae7581e112fa1f5094ff26
Paul Ebert <paul.ebert@code.berlin>
2024-07-22 16:36:23 +0200

    error middleware generic

--- src/middleware/errorMiddleware.ts
+++ src/middleware/errorMiddleware.ts
@@ -1,17 +1,28 @@
 import { NextFunction, Request, Response } from "express";
 import { ZodError } from "zod";
 import { logger } from "../utils/logger";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
-export function zodErrorMiddleware(
-  err: ZodError | Error,
+export function ErrorMiddleware<ErrorT>(
+  err: ErrorT,
   req: Request,
   res: Response,
-  next: NextFunction,
 ) {
   if (err instanceof ZodError) {
-    logger.error(`Error occured on ${req.path}`);
+    logger.error(`Zod error occured on ${req.path}`);
     logger.error(err.issues);
-    return res.status(401).json(err.issues);
+
+    return res.status(402).json(err.issues);
+  } else if (err instanceof PrismaClientKnownRequestError) {
+    logger.error(`Prisma known request error on ${req.path}`);
+    logger.error(`${err.code}: ${err.name}`);
+
+    return res.status(400).json(err.code);
+  } else if (!(err instanceof Error)) {
+    logger.warn(
+      "Error landed here without being an error, unhandled exception!",
+    );
+    logger.warn(err);
   }
-  res.status(400).json({ rezo: "ja lol ey" });
+  return res.status(400).json({ "Unknown Error": err });
 }
