7d7de444e68504a0910d3d9f7e83d8acf34f3e57
Paul Ebert <paul.ebert@code.berlin>
2024-07-26 23:49:40 +0200

    authservice hashpasswords, match generate token and verify function

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,3 +1,5 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import { checkAccountExistsByUsername } from "../helpers/accountExists";
 import prisma from "../lib/db";
 import { logger } from "../utils/logger";
@@ -9,6 +11,7 @@ export async function createAccount(username: string, password: string) {
   }
 
   try {
+    password = await hashPassword(password);
     const accountCreated = await prisma.user.create({
       data: { username, password },
     });
@@ -17,4 +20,37 @@ export async function createAccount(username: string, password: string) {
     logger.error((err as Error).name);
     throw new Error((err as Error).name);
   }
+}
+
+export async function hashPassword(password: string) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    return hashedPassword;
+  } catch (err) {
+    logger.error(err);
+    throw new Error((err as Error).message);
+  }
+}
+
+export async function doesHashMatchPassword(hash: string, password: string) {
+  return await bcrypt.compare(password, hash);
+}
+
+export function generateToken(payload: string) {
+  const secret = process.env.JWT_SECRET || "";
+
+  const token = jwt.sign({ sub: payload }, secret, {
+    expiresIn: "1h",
+  });
+
+  return token;
+}
+
+export async function verifySignature(token: string) {
+  try {
+    const secret = process.env.SECRET || "";
+    const decodedToken = jwt.verify(token, secret);
+  } catch (e) {}
 }
